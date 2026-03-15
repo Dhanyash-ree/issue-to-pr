067039bc6a8ab5904fa5a5caea5168ed5b85c4a9
Dhanyashree <dhanyashree4741@gmail.com>
2026-03-15 20:59:46 +0530

    AI generated code fix

--- formvalidator.js
+++ formvalidator.js
@@ -1,11 +1,4 @@
-function validateEmail(email){
-
-  if(!email){
-    return false;
-  }
-
-  const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-  return pattern.test(email);
-
+function validateEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
